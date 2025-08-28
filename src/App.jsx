fd52b047db2a5b2f42a15db0edc5c8d8569a6c0a
Nuriddinbek Obidjonov <nuriddinbek.obidjonov@gmail.com>
2025-08-28 14:33:35 +0500

    Hero section finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
-import React from "react";
 import Navbar from "./sections/Navbar";
 import Hero from "./sections/Hero";
+import About from "./sections/About";
 
 function App() {
   return (
     <div className="container mx-auto max-w-7xl">
       <Navbar />
       <Hero />
+      <About />
       <section className="h-screen"></section>
       <section className="h-screen"></section>
       <section className="h-screen"></section>
