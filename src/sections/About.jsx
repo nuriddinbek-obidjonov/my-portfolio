08c28fff4d2db6d7d37d7383bca90976459e051f
Nuriddinbek Obidjonov <nuriddinbek.obidjonov@gmail.com>
2025-08-28 15:41:31 +0500

    about section is under process

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -1,7 +1,54 @@
-import React from "react";
+import Card from "../components/Card";
 
-function About() {
-  return <div>About</div>;
-}
+const About = () => {
+  return (
+    <section className="c-space section-spacing">
+      <h2 className="text-heading">About Me</h2>
+      <div className="grid grid-cols-1 gap-4 mt-12 md:grid-cols-6 md:auto-rows-[18rem]">
+        {/* Grid 1 */}
+        <div className="flex items-end grid-default-color grid-1">
+          <img
+            src="assets/coding-pov.png"
+            alt="image"
+            className="absolute scale-[1.75] -right-[5rem] -top-[1rem] md:scale-[3] md:left-50 md:inset-y-10 lg:scale-[2.5]"
+          />
+          <div className="z-10">
+            <p className="headtext">Hi I'm Nuriddinbek Obidjonov</p>
+            <p className="subtext">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Non,
+              ipsum nisi atque magni ea laborum cupiditate, facere ab
+              accusantium sequi possimus labore ex tenetur velit deserunt fugit
+              exercitationem, aliquid at iure amet esse! Repudiandae sed in
+              itaque ea at. Suscipit alias assumenda sequi reprehenderit iusto,
+              unde ducimus illo eligendi at!
+            </p>
+            <div className="absolute inset-x-0 pointer-events-none -bottom-4 h-1/2 sm:h-1/3 bg-gradient-to-t from-indigo"></div>
+          </div>
+        </div>
+        {/* Grid 2 */}
+        <div className="grid-default-color grid-2">
+          <div className="flex items-center justify-center h-full w-full">
+            <p className="flex items-end text-5xl text-gray-500">
+              CODE IS CRAFT
+            </p>
+            <Card style={{rotate: "75deg", top: "30%", left: "20%"}} text="GRASP" />
+            <Card style={{rotate: "30deg", top: "60%", left: "45%"}} text="Design Principles" />
+            <Card style={{rotate: "90deg", bottom: "30%", left: "70%"}} text="SOLID" />
+            <Card style={{rotate: "-45deg", top: "55%", left: "0%"}} text="Design Patterns" />
+            <Card style={{rotate: "20deg", top: "10%", left: "38%"}} text="SRP" />
+            <Card style={{rotate: "30deg", top: "70%", left: "70%"}} image="assets/logos/csharp-pink.png" />
+            <Card style={{rotate: "-45deg", top: "70%", left: "25%"}} image="assets/logos/dotnet-pink.png" />
+          </div>
+        </div>
+        {/* Grid 3 */}
+        <div className="grid-black-color grid-3"></div>
+        {/* Grid 4 */}
+        <div className="grid-special-color grid-4"></div>
+        {/* Grid 5 */}
+        <div className="grid-default-color grid-5"></div>
+      </div>
+    </section>
+  );
+};
 
 export default About;
