8671b4ff98fd6ae800eab0d8210dabbb6f7da5ef
Nuriddinbek Obidjonov <nuriddinbek.obidjonov@gmail.com>
2025-09-01 20:09:55 +0500

    personal informations are being mathced with my own

--- src/constants/index.js
+++ src/constants/index.js
@@ -225,9 +225,9 @@ export const myProjects = [
 
 export const mySocials = [
   {
-    name: "WhatsApp",
-    href: "",
-    icon: "/assets/socials/whatsApp.svg",
+    name: "Telegram",
+    href: "https://t.me/nuriddinbek_1/",
+    icon: "/assets/socials/telegram.svg",
   },
   {
     name: "Linkedin",
@@ -244,8 +244,8 @@ export const mySocials = [
 export const experiences = [
   {
     title: "Software Developer",
-    job: "Security & Defense Projects",
-    date: "2021-2023",
+    job: "LIMSA",
+    date: "2025/01-present",
     contents: [
       "Enhanced application security and developed new features, adhering to standards set by the Passive Defense Organization and National Cyberspace Center.",
       "Designed and implemented intuitive map interfaces using MapsUI, enhancing user experience and enabling seamless interactive map integration.",
@@ -255,24 +255,14 @@ export const experiences = [
     ],
   },
   {
-    title: "Back-End Developer",
-    job: "Car Manufacture",
-    date: "2023-2024",
+    title: "Front-end Developer",
+    job: "ITA",
+    date: "2024/03-2024/12",
     contents: [
       "Engineered systems for large-scale data ingestion and analysis, ensuring efficient data processing and storage.",
-      "Developed back-end systems enabling vehicle-to-cloud communication for telemetry, diagnostics, and remote control:",
       "✅ Implemented secure APIs, following ISO 26262 automotive safety standards.",
       "✅ Ensured data privacy for customers and partners through industry-compliant protocols.",
       "✅ Delivered remote features like over-the-air updates, real-time tracking, and remote start capabilities.",
-    ],
-  },
-  {
-    title: "Freelance Developer",
-    job: "Self-Employed",
-    date: "2025-Present",
-    contents: [
-      "Created a personal portfolio using Three.js, React, Vite, and WebAPI to showcase technical expertise.",
-      "Continuously enhancing technical skills and expanding expertise in modern web development and back-end technologies.",
     ],
   },
 ];
