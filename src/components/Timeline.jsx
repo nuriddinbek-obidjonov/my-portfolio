dd674c67fcf176a83ef66e629e6ecc1021d32568
Nuriddinbek Obidjonov <nuriddinbek.obidjonov@gmail.com>
2025-10-27 13:25:37 +0500

    timeline

--- src/components/Timeline.jsx
+++ src/components/Timeline.jsx
@@ -18,7 +18,7 @@ const Timeline = ({ data }) => {
 
   const { scrollYProgress } = useScroll({
     target: containerRef,
-    offset: ["start 30%", "end 50%"],
+    offset: ["start 10%", "end 50%"],
   });
 
   const heightTransform = useTransform(scrollYProgress, [0, 1], [0, height]);
